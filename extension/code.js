47b0240251b319beb8d37a1ade5e17ed62064cc7
Zoix01 <fanda.magic@gmail.com>
2025-03-31 09:40:16 +0200

    funkcni update

--- extension/code.js
+++ extension/code.js
@@ -29,35 +29,23 @@ let zacatekSlova = null
 let konecSlova = null
 
 function opravaSlov(slovo) {
-  const hledaniSlova = document.getBody().findText(slovo.original)
+  let hledaniSlova = document.getBody().findText(slovo.original)
   if (hledaniSlova !== null) {
     naOpraveni = hledaniSlova.getElement().asText()
     zacatekSlova = hledaniSlova.getStartOffset()
     konecSlova = hledaniSlova.getEndOffsetInclusive()
     naOpraveni.setStrikethrough(zacatekSlova, konecSlova, true)
-    naOpraveni.insertText(konecSlova + 1, slovo.oprava)
+    naOpraveni.insertText(konecSlova + 1, " " + slovo.oprava)
     const delkaOpravenehoSlova = konecSlova + slovo.oprava.length
-    naOpraveni.setStrikethrough(konecSlova + 1, delkaOpravenehoSlova, false)
+    naOpraveni.setStrikethrough(konecSlova + 1, delkaOpravenehoSlova + 1, false)
+  }
+  hledaniSlova = document.getBody().findText(slovo.original)
   naOpraveni = hledaniSlova.getElement().asText()
   zacatekSlova = hledaniSlova.getStartOffset()
-  naOpraveni.
-
-}}
+  naOpraveni.insertText(zacatekSlova, "*")
+}
 
 function smazaniSlov() {
-  console.log("1")
-  const paragraphs = document.getBody().getParagraphs()
-  paragraphs.forEach(paragraph => {
-    const textNaEdit = paragraph.editAsText()
-    const text = textNaEdit.getText()
-    for (let i = text.length - 1; i >= 0; i --) {
-      const attributes = textNaEdit.getAttributes(i)
-      console.log(i)
-      if(attributes[DocumentApp.Attribute.STRIKETHROUGH]) {
-        console.log("bruh")
-        textNaEdit.deleteText(i, i)
-      }
-    }
-  })
+  body.replaceText("\\*\\S+", "")
 }
 
