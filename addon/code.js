ce01d3e61a228c300f04aff59b728f90337a16e2
Zoix01 <fanda.magic@gmail.com>
2025-04-06 10:14:23 +0200

    renaming files

--- addon/code.js
+++ addon/code.js
@@ -1,32 +1,33 @@
+
 function onOpen(e) {
   DocumentApp.getUi().createAddonMenu()
       .addItem('Start', 'showSidebar')
-      .addToUi();
+      .addToUi()
 }
 function onInstall(e) {
-  onOpen(e);
+  onOpen(e)
 }
 function showSidebar() {
   const html = HtmlService.createTemplateFromFile('sidebar').evaluate().setTitle("Slohobot")
   DocumentApp.getUi()
     .showSidebar(html)
 }
+
 const document = DocumentApp.getActiveDocument()
 const body = document.getBody()
+let naOpraveni = null
+let zacatekSlova = null
+let konecSlova = null
+
+function extractTextForServer() {
+  return document.getBody().getText()
+}
 
 function extractTextFromParagraphs() {
   const paragraphs = document.getBody().getParagraphs()
   const paragraphsText = paragraphs.map( p => {return p.getText()})
   return paragraphsText
 }
-
-function extractTextForServer() {
-  return document.getBody().getText()
-}
-
-let naOpraveni = null
-let zacatekSlova = null
-let konecSlova = null
 
 function opravaSlov(slovo) {
   let hledaniSlova = document.getBody().findText(slovo.original)
@@ -40,12 +41,15 @@ function opravaSlov(slovo) {
     naOpraveni.setStrikethrough(konecSlova + 1, delkaOpravenehoSlova + 1, false)
   }
   hledaniSlova = document.getBody().findText(slovo.original)
-  naOpraveni = hledaniSlova.getElement().asText()
-  zacatekSlova = hledaniSlova.getStartOffset()
-  naOpraveni.insertText(zacatekSlova, "*")
+  if (hledaniSlova !== null) {
+    naOpraveni = hledaniSlova.getElement().asText()
+    zacatekSlova = hledaniSlova.getStartOffset()
+    naOpraveni.insertText(zacatekSlova, "*")
+  }
 }
 
 function smazaniSlov() {
   body.replaceText("\\s+\\*\\S+", "")
 }
+
 
